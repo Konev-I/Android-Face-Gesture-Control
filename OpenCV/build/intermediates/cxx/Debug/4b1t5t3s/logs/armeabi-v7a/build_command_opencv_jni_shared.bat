804135133408a34cabc7fc638dafd17fbdb45cd0
Konev_I <kva19.2000@mail.ru>
2024-05-14 21:12:12 +0300

    OpenCV files changes

---
+++ OpenCV/build/intermediates/cxx/Debug/4b1t5t3s/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\79028\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\79028\\AndroidStudioProjects\\Application\\OpenCV\\.cxx\\Debug\\4b1t5t3s\\armeabi-v7a" ^
+  opencv_jni_shared
